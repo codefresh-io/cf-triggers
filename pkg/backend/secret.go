2376a3b1733c542a6c5e2547b4332de7e4982e09
Alexei Ledenev <alexei.led@gmail.com>
2017-12-26 12:38:35 +0200

    refactor towards SOLID

--- pkg/backend/secret.go
+++ pkg/backend/secret.go
@@ -1,10 +1,15 @@
 package backend
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
 	"errors"
+	"hash"
 
 	"github.com/codefresh-io/hermes/pkg/model"
-	"github.com/codefresh-io/hermes/pkg/util"
 )
 
 // SecretChecker checks secret or HMAC signature
@@ -16,11 +21,26 @@ func NewSecretChecker() model.SecretChecker {
 	return &SecretChecker{}
 }
 
+// helper function to check HMAC sha1/256/512 signatures
+func checkHmacSignature(message, signature, secret string) bool {
+	hashes := [](func() hash.Hash){sha1.New, sha256.New, sha512.New}
+	for _, h := range hashes {
+		hash := hmac.New(h, []byte(secret))
+		hash.Write([]byte(message))
+		expectedMAC := hash.Sum(nil)
+		sig, _ := hex.DecodeString(signature)
+		if hmac.Equal(sig, expectedMAC) {
+			return true
+		}
+	}
+	return false
+}
+
 // Validate secret or HMAC signature
 func (s *SecretChecker) Validate(message string, secret string, key string) error {
 	if secret != key {
 		// try to check signature
-		if util.CheckHmacSignature(message, secret, key) {
+		if checkHmacSignature(message, secret, key) {
 			return nil
 		}
 		return errors.New("invalid secret or HMAC signature")
