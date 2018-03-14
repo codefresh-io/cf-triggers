c0d7aad61709279e130e07c9e8ccbb00569d314b
olegs-codefresh <olegs@codefresh.io>
2018-03-14 14:32:23 +0200

    Add account id to internal http request of a pipeline

--- pkg/codefresh/pipeline.go
+++ pkg/codefresh/pipeline.go
@@ -120,7 +120,7 @@ func (api *APIEndpoint) getPipeline(account, id string) (*Pipeline, error) {
 	if api.internal {
 		// use internal cfapi - another endpoint and need to ass account
 		log.Debug("using internal cfapi")
-		resp, err = api.endpoint.New().Get(fmt.Sprint("api/pipelines/", id)).ReceiveSuccess(pipeline)
+		resp, err = api.endpoint.New().Get(fmt.Sprint("api/pipelines/", account, "/", id)).ReceiveSuccess(pipeline)
 	} else {
 		// use public cfapi
 		log.Debug("using public cfapi")
